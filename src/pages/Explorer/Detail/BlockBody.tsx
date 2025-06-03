3d5c6f078be440c263846edd952df5a50fcdead1
Carlo Sala <carlosalag@protonmail.com>
2025-06-03 10:39:48 +0200

    wip

--- src/pages/Explorer/Detail/BlockBody.tsx
+++ src/pages/Explorer/Detail/BlockBody.tsx
@@ -12,13 +12,12 @@ import { BlockInfo, blockInfoState$ } from "../block.state"
 import { BlockEvents } from "./BlockEvents"
 import { BlockStorageDiff } from "./BlockStorageDiff"
 import { ApplyExtrinsicEvent, Extrinsic } from "./Extrinsic"
-import { createExtrinsicCodec, DecodedExtrinsic } from "./extrinsicDecoder"
+import { getExtrinsicDecoder } from "@polkadot-api/tx-utils"
+import { getHashParams } from "@/hashParams"
 
 const blockExtrinsics$ = state((hash: string) => {
   const decoder$ = runtimeCtx$.pipe(
-    map(({ dynamicBuilder, lookup }) =>
-      createExtrinsicCodec(dynamicBuilder, lookup),
-    ),
+    map(({ metadataRaw }) => getExtrinsicDecoder(metadataRaw)),
   )
   const body$ = blockInfoState$(hash).pipe(
     filter((v) => !!v),
@@ -28,13 +27,13 @@ const blockExtrinsics$ = state((hash: string) => {
   )
 
   return combineLatest([body$, decoder$]).pipe(
-    map(([body, decoder]): Array<DecodedExtrinsic> => body.map(decoder)),
+    map(([body, decoder]) => body.map(decoder)),
     // Assuming the body or the decoder won't change or won't have any effect.
     take(1),
   )
 }, [])
 
-type Tab = "signed" | "unsigned" | "events" | "diff"
+type Tab = "tx" | "events" | "diff"
 export const BlockBody: FC<{
   block: BlockInfo
 }> = ({ block }) => {
@@ -43,7 +42,7 @@ export const BlockBody: FC<{
   const extrinsics = useStateObservable(blockExtrinsics$(hash ?? ""))
 
   const location = useLocation()
-  const hashParams = new URLSearchParams(location.hash.slice(1))
+  const hashParams = getHashParams(location)
   const eventParam = hashParams.get("event")
   const defaultEventOpen =
     eventParam && block.events ? block.events[Number(eventParam)] : null
@@ -59,27 +58,16 @@ export const BlockBody: FC<{
 
   if (!block) return <Loading>Loading block…</Loading>
 
-  const groupedExtrinsics = groupBy(
-    extrinsics.map((e, index) => ({ ...e, index })),
-    (e) => (e.signed ? "signed" : ("unsigned" as const)),
-  )
-
   // This is done separately in case the extrinsics/events have not fully loaded yet.
   const getDefaultTab = (): Tab => {
     if (defaultEventOpen) {
-      return defaultEventOpen.phase.type === "ApplyExtrinsic"
-        ? extrinsics[defaultEventOpen.phase.value]?.signed
-          ? "signed"
-          : "unsigned"
-        : "events"
+      const extrinsic =
+        defaultEventOpen.phase.type === "ApplyExtrinsic"
+          ? extrinsics[defaultEventOpen.phase.value]
+          : null
+      return extrinsic ? "tx" : "events"
     }
-    if (groupedExtrinsics.signed?.length) {
-      return "signed"
-    }
-    if (groupedExtrinsics.unsigned?.length) {
-      return "unsigned"
-    }
-    return "events"
+    return extrinsics.length ? "tx" : "events"
   }
 
   const effectiveTab = selectedTab ?? getDefaultTab()
@@ -98,21 +86,10 @@ export const BlockBody: FC<{
               "disabled:text-secondary-foreground/50 disabled:bg-secondary/50 disabled:pointer-events-none",
               "data-[state=active]:font-bold data-[state=active]:text-secondary-foreground",
             )}
-            value="signed"
-            disabled={!groupedExtrinsics.signed?.length}
+            value="tx"
+            disabled={!extrinsics.length}
           >
-            Signed
-          </Tabs.Trigger>
-          <Tabs.Trigger
-            className={twMerge(
-              "bg-secondary text-secondary-foreground/80 px-4 py-2 hover:text-polkadot-500 border-t border-r border-polkadot-200",
-              "disabled:text-secondary-foreground/50 disabled:pointer-events-none",
-              "data-[state=active]:font-bold data-[state=active]:text-secondary-foreground",
-            )}
-            value="unsigned"
-            disabled={!groupedExtrinsics.unsigned?.length}
-          >
-            Unsigned
+            Extrinsics
           </Tabs.Trigger>
           <Tabs.Trigger
             className={twMerge(
@@ -141,26 +118,14 @@ export const BlockBody: FC<{
             </Tabs.Trigger>
           )}
         </Tabs.List>
-        <Tabs.Content value="signed" className="py-2">
+        <Tabs.Content value="tx" className="py-2">
           <ol>
-            {groupedExtrinsics.signed?.map((extrinsic) => (
+            {extrinsics.map((extrinsic, idx) => (
               <Extrinsic
-                key={extrinsic.index}
+                key={idx}
                 extrinsic={extrinsic}
                 highlightedEvent={defaultEventOpen}
-                events={eventsByExtrinsic?.[extrinsic.index] ?? []}
-              />
-            ))}
-          </ol>
-        </Tabs.Content>
-        <Tabs.Content value="unsigned" className="py-2">
-          <ol>
-            {groupedExtrinsics.unsigned?.map((extrinsic) => (
-              <Extrinsic
-                key={extrinsic.index}
-                extrinsic={extrinsic}
-                highlightedEvent={defaultEventOpen}
-                events={eventsByExtrinsic?.[extrinsic.index] ?? []}
+                events={eventsByExtrinsic?.[idx] ?? []}
               />
             ))}
           </ol>
