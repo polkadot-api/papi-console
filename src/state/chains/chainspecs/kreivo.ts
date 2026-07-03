2fde4934322280e6ccb36c2b00e807c1c9c53711
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2026-07-03 06:09:47 +0000

    minor: add `boot.kippu.rocks` to the list.

--- src/state/chains/chainspecs/kreivo.ts
+++ src/state/chains/chainspecs/kreivo.ts
@@ -5,7 +5,8 @@ export const chainSpec = JSON.stringify({
   bootNodes: [
     "/dns/boot-1.kreivo.bloque.network/tcp/443/wss/p2p/12D3KooWEht3GDE23nGEhpyNgAJ6B2GiGT9hquBDko6PtWuFRRB8",
     "/dns/boot-2.kreivo.bloque.network/tcp/443/wss/p2p/12D3KooWLqz8Aj5g3snhLx2VZJzfAMNMZbwBrNs8LJGjY1ks4LcG",
-    "/dns/boot-3.kreivo.bloque.network/tcp/443/wss/p2p/12D3KooWDAtFSYCotCmX5DtXTP2xsHvxtzfUSrVsgegFXopCzUBU"
+    "/dns/boot-3.kreivo.bloque.network/tcp/443/wss/p2p/12D3KooWDAtFSYCotCmX5DtXTP2xsHvxtzfUSrVsgegFXopCzUBU",
+    "/dns/boot.kreivo.kippu.rocks/tcp/443/wss/p2p/12D3KooWNpsPXUKvg9Gk9te54r4xodSPzVgLUkrfhDPpYXNepCb7"
   ],
   properties: {
     ss58Format: 2,
